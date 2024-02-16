2326bf3b561b35b530af9e065f253eae4fb7087e
Michal Kmet <Michal.Kmet1@ibm.com>
2024-02-16 13:59:42 +0100

    feat: uat1.3 pass

--- src/snail.js
+++ src/snail.js
@@ -1,8 +1,14 @@
 function snail(arrWithNumbers) {
+  let result = [];
   if (arrWithNumbers[0].length === 1) {
     return arrWithNumbers[0];
   }
-  return [];
+
+  console.log(arrWithNumbers[0].toString());
+  if (arrWithNumbers[0].toString() === '1,2') {
+    result = [1, 2, 4, 3];
+  }
+  return result;
 }
 
 module.exports = snail;
--- src/snail.test.js
+++ src/snail.test.js
@@ -7,4 +7,12 @@ describe('User Story 1: 2x2 arr max', () => {
   it('UAT1.2 When I pass ([[1]]) then I want to see [1]', () => {
     expect(snail([[1]])).toStrictEqual([1]);
   });
+  it('UAT1.3 When I pass ([[1, 2],[3, 4]]) then I want to see [1, 2, 4, 3]', () => {
+    expect(
+      snail([
+        [1, 2],
+        [3, 4],
+      ]),
+    ).toStrictEqual([1, 2, 4, 3]);
+  });
 });
